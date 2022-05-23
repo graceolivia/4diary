d44aebc2c191838961db99f1bca310bae2fdb673
Grace <gracelidinskysmith@gmail.com>
2022-05-23 09:27:48 -0400

    CORS

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module IncidentTracker
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -20,5 +21,15 @@ module IncidentTracker
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource(
+          '*',
+          headers: :any,
+          methods: [:get, :patch, :put, :delete, :post, :options]
+          )
+      end
+    end
   end
 end
