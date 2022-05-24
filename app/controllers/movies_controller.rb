a1f086304108e61a98c305345848fea7664582e5
Grace <gracelidinskysmith@gmail.com>
2022-05-24 14:01:52 -0400

    Change phrasing

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,3 +1,4 @@
-class MoviesController < JSONAPI::ResourceController
+class MoviesController < ActionController::Base
+  include JSONAPI::ActsAsResourceController
   skip_before_action :verify_authenticity_token
 end
